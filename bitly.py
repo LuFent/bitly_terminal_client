d233cf0ca44202fe2033d3d0f012cf3902fa71be
LuFent <vladmarchenkosteam@gmail.com>
2021-11-07 16:00:18 +0300

    Code formated

--- bitly.py
+++ bitly.py
@@ -12,6 +12,11 @@ def is_bitlink(url_id, headers):
     return requests.get(url, headers=headers).ok
 
 
+def get_bitly_url_id(url):
+    parsed_url = urlparse(url)
+    return parsed_url.netloc + parsed_url.path
+
+
 def shorten_link(long_url, header):
     payload = {"long_url": long_url}
     url = "https://api-ssl.bitly.com/v4/shorten"
@@ -38,15 +43,14 @@ def main():
     user_url = args.user_url
 
     parsed_url = urlparse(user_url)
-    bitly_url_id = "".join(parsed_url[1:3])
 
-    if not urlparse(user_url)[0] == "https" or urlparse(user_url)[0] == "http":
+    if not parsed_url.scheme:
         print("Url without protocol")
         sys.exit()
 
     try:
-        if is_bitlink(bitly_url_id, header):
-            print(f"You entered BitLink, count of clicks on it: {get_clicks(bitly_url_id, header)}")
+        if is_bitlink(get_bitly_url_id(user_url), header):
+            print(f"You entered BitLink, count of clicks on it: {get_clicks(get_bitly_url_id(user_url), header)}")
         else:
             print(f"Bitlink created: {shorten_link(user_url, header)}")
 
